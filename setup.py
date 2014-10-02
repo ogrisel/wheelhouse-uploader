c7d7532ebba93dcc2c47a4b043258a3b5d8c6940
Olivier Grisel <olivier.grisel@ensta.org>
2014-10-02 13:25:46 +0200

    Release 0.5.0

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ except ImportError:
 
 setup(
     name="wheelhouse-uploader",
-    version="0.4.3",
+    version="0.5.0",
     description="Upload wheels to any cloud storage supported by Libcloud",
     maintainer="Olivier Grisel",
     maintainer_email="olivier.grisel@ensta.org",
